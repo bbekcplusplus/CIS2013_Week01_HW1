3fb39dfec75613b07d359a7e93fe38d5d4c57244
Bibek Gupta <bibek.gupta@okstate.edu>
2018-01-24 13:08:39 -0600

    Initial commit

---
+++ CIS2013_Week01_HW1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ \n\n";
+    cout << "   Bibek Gupta \n\n";
+    cout << "   Student, Oklahoma State University-Oklahoma City \n\n";
+    cout << "   Major: Computer Information Systems \n\n";
+    cout << "   6413 N Warren Ave                Ph: (405) 430-6995 \n\n";
+    cout << "   Oklahoma City, OK                E: bibek.gupta@okstate.edu \n";
+    cout << "                                    G: github.com/bbekcplusplus \n";
+    cout << "                                    S: bbekslack \n";
+    cout << "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^";
+    	
+	return 0;
+	
+	
+	
+}
